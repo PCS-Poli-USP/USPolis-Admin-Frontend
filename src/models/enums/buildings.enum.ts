deb4fe812b0b75a99b13ca90b8d84daa63481c82
aguenaro <rodrigoaguena@gmail.com>
2023-06-18 16:42:33 -0300

    add the rest of buildings

--- src/models/enums/buildings.enum.ts
+++ src/models/enums/buildings.enum.ts
@@ -1,4 +1,9 @@
 export enum Buildings {
   BIENIO = 'Biênio',
   ELETRICA = 'Elétrica',
+  MECANICA = 'Mecânica',
+  PRODUCAO = 'Produção',
+  CIVIL = 'Civil',
+  ADMINISTRACAO = 'Administração',
+  METALURGICA = 'Metalúrgica',
 }
