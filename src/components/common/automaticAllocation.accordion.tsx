396e892338ce8631940e5d21af97779f1f52b988
FastShowDev <zlfastshow@hotmail.com>
2024-01-19 16:49:27 -0300

    Updage classroom visualization in automatic allocation

--- src/components/common/automaticAllocation.accordion.tsx
+++ src/components/common/automaticAllocation.accordion.tsx
@@ -7,11 +7,10 @@ import {
   Box,
   Text,
   HStack,
-  Icon,
-  Flex,
+  Link,
 } from '@chakra-ui/react';
 import { CalendarIcon } from '@chakra-ui/icons';
-import { BsBookHalf, BsHouseFill } from "react-icons/bs";
+import { BsBookHalf, BsHouseFill } from 'react-icons/bs';
 
 import Classroom from 'models/classroom.model';
 import Event from 'models/event.model';
@@ -19,7 +18,6 @@ import Event from 'models/event.model';
 import { useEffect, useState } from 'react';
 import { weekDaysFormatter } from 'utils/classes/classes.formatter';
 import ClassroomsService from 'services/classrooms.service';
-
 
 interface AutomaticAllocationAccordionProps {
   allocated: Event[];
@@ -30,7 +28,6 @@ export default function AutomaticAllocationAccordion({
   allocated,
   unallocated,
 }: AutomaticAllocationAccordionProps) {
-
   const classroomService = new ClassroomsService();
   const [classrooms, setClassrooms] = useState<Classroom[]>([]);
 
@@ -41,28 +38,37 @@ export default function AutomaticAllocationAccordion({
 
   function fetchClassrooms() {
     classroomService.list().then((it) => {
-      it.data.sort((a, b) => {
-        if (a.classroom_name < b.classroom_name) return -1;
-        if (a.classroom_name > b.classroom_name) return 1;
-        return 0;
-      });
+      it.data.sort(sortClassroomByName);
       setClassrooms(it.data);
     });
   }
 
-  function sortByClassCode(a: Event, b: Event) {
+  function sortClassroomByName(a: Classroom, b: Classroom) {
+    if (a.classroom_name < b.classroom_name) return -1;
+    if (a.classroom_name > b.classroom_name) return 1;
+    return 0;
+  }
+
+  function sortEventByClassCode(a: Event, b: Event) {
     if (a.subject_code < b.subject_code) return -1;
     if (a.subject_code > b.subject_code) return 1;
     return 0;
   }
 
-  allocated.sort(sortByClassCode)
-  unallocated.sort(sortByClassCode);
+  allocated.sort(sortEventByClassCode);
+  unallocated.sort(sortEventByClassCode);
 
   return (
-    <Accordion defaultIndex={unallocated && unallocated.length > 0 ? [1] : [0]} allowMultiple={true} allowToggle={true}>
+    <Accordion
+      defaultIndex={unallocated && unallocated.length > 0 ? [1] : [0]}
+      allowMultiple={true}
+    >
       <AccordionItem>
-        <AccordionButton bg={'uspolis.blue'} color={'blackAlpha.900'} fontWeight={'bold'}>
+        <AccordionButton
+          bg={'uspolis.blue'}
+          color={'blackAlpha.900'}
+          fontWeight={'bold'}
+        >
           <Box as='span' flex='1' textAlign='left'>
             Turmas Alocadas
           </Box>
@@ -70,11 +76,13 @@ export default function AutomaticAllocationAccordion({
         </AccordionButton>
         <AccordionPanel pb={4}>
           {allocated.map((value, index) => (
-            <HStack spacing={3}>
+            <HStack spacing={3} key={index}>
               <BsBookHalf />
               <Text>{`${value.subject_code} - ${value.class_code}, ${value.vacancies} vagas`}</Text>
               <CalendarIcon />
-              <Text>{`${weekDaysFormatter(value.week_day)}, ${value.start_time} às ${value.end_time}`}</Text>
+              <Text>{`${weekDaysFormatter(value.week_day)}, ${
+                value.start_time
+              } às ${value.end_time}`}</Text>
               <BsHouseFill />
               <Text>{`${value.classroom}`}</Text>
             </HStack>
@@ -86,40 +94,53 @@ export default function AutomaticAllocationAccordion({
         <AccordionItem>
           <AccordionButton bg={'red.500'} color={'black'} fontWeight={'bold'}>
             <Box as='span' flex='1' textAlign='left'>
-            Turmas Não Alocadas
+              Turmas Não Alocadas
             </Box>
             <AccordionIcon />
           </AccordionButton>
           <AccordionPanel pb={4}>
-          {unallocated.map((value, index) => (
-            <HStack spacing={3}>
-              <BsBookHalf />
-              <Text>{`${value.subject_code} - ${value.class_code}, ${value.vacancies} vagas`}</Text>
-              <CalendarIcon />
-              <Text>{`${weekDaysFormatter(value.week_day)}, ${value.start_time} às ${value.end_time}`}</Text>
-            </HStack>
-          ))}
+            {unallocated.map((value, index) => (
+              <HStack spacing={3} key={index}>
+                <BsBookHalf />
+                <Text>{`${value.subject_code} - ${value.class_code}, ${value.vacancies} vagas`}</Text>
+                <CalendarIcon />
+                <Text>{`${weekDaysFormatter(value.week_day)}, ${
+                  value.start_time
+                } às ${value.end_time}`}</Text>
+              </HStack>
+            ))}
           </AccordionPanel>
         </AccordionItem>
       ) : undefined}
 
       <AccordionItem>
-        <AccordionButton bg={'uspolis.blue'} color={'blackAlpha.900'} fontWeight={'bold'}>
+        <AccordionButton
+          bg={'uspolis.blue'}
+          color={'blackAlpha.900'}
+          fontWeight={'bold'}
+        >
           <Box as='span' flex='1' textAlign='left'>
             Salas cadastradas
           </Box>
           <AccordionIcon />
         </AccordionButton>
         <AccordionPanel pb={4}>
-          {classrooms.map((value, index) => (
-            <HStack>
-              <BsHouseFill />
-              <Text key={index}>{`${value.classroom_name} - ${value.capacity} de capacidade`}</Text>
-            </HStack>
-          ))}
+          {classrooms.length > 0 ? (
+            classrooms.map((value, index) => (
+              <HStack spacing={3} key={index}>
+                <BsHouseFill />
+                <Text>{`${value.classroom_name} - ${value.capacity} capacidade`}</Text>
+              </HStack>
+            ))
+          ) : (
+            <Text>Sem salas cadastradas, ir para{' '}
+              <Link href='/classrooms'>
+                criação de salas
+              </Link>
+            </Text>
+          )}
         </AccordionPanel>
       </AccordionItem>
-
     </Accordion>
   );
 }
