56e4fd08c4b2977cf25b9dd1ec6d11d8e988733a
FastShowDev <zlfastshow@hotmail.com>
2024-02-26 04:09:01 -0300

    Create sort classrooms function

--- src/utils/sorter.ts
+++ src/utils/sorter.ts
@@ -1,5 +1,6 @@
 import Class from 'models/class.model';
 import { Building } from 'models/building.model';
+import Classroom from 'models/classroom.model';
 
 export function sortClasses(a: Class, b: Class) {
   if (a.subject_code === b.subject_code) {
@@ -18,4 +19,10 @@ export function sortBuilding(a: Building, b: Building) {
   if (a.name < b.name) return -1;
   else if (a.name > b.name) return 1;
   return 0;
+}
+
+export function sortClassrooms(a: Classroom, b: Classroom) {
+  if (a.classroom_name < b.classroom_name) return -1;
+  if (a.classroom_name === b.classroom_name) return 0;
+  return 1;
 }
