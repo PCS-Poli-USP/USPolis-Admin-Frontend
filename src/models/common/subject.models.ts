fb7b8c03b6e7428f3a76d7d7208c4d82fdef77b4
Gabriel Di Vanna Camargo <zlFastShow@hotmail.com>
2024-05-19 21:42:42 -0300

    Rename models folder

--- src/models/common/subject.models.ts
+++ src/models/common/subject.models.ts
@@ -12,16 +12,3 @@ export interface Subject {
   activation: string;
   desactivation?: string;
 }
-
-export interface CreateSubject {
-  code: string;
-  name: string;
-  professors: Array<string>;
-  type: string;
-  class_credit: number;
-  work_credit: number;
-  activation: string;
-  desactivation?: string;
-}
-
-export interface UpdateSubject extends CreateSubject {}
