df0acd1d451abd2f57558335ed0f27e2dfb5f6a4
Gabriel Di Vanna Camargo <zlFastShow@hotmail.com>
2024-06-04 07:51:11 -0300

    Create MultiSelect Form Component

---
+++ src/components/common/form/MultiSelect/index.tsx
@@ -0,0 +1,60 @@
+import { FormLabel, FormControl, FormErrorMessage } from '@chakra-ui/react';
+import { FieldProps } from 'models/interfaces';
+import { Controller, useFormContext } from 'react-hook-form';
+import Select from 'react-select';
+
+type Option = {
+  label: string;
+  value: string | number;
+};
+
+interface MultiSelectProps extends FieldProps {
+  options: Option[];
+  value?: Option[];
+  loading?: boolean;
+}
+
+export function MultiSelect({
+  label,
+  name,
+  options,
+  disabled = false,
+  loading = false,
+  value = undefined,
+  placeholder = undefined,
+}: MultiSelectProps) {
+  const {
+    control,
+    formState: { errors },
+  } = useFormContext();
+
+  return (
+    <Controller
+      control={control}
+      name={'categories_ids'}
+      render={({ field }) => (
+        <FormControl isInvalid={!!errors[name]}>
+          <FormLabel alignSelf='flex-start'>{label}</FormLabel>
+          <Select
+            value={value}
+            isLoading={loading}
+            isDisabled={disabled}
+            placeholder={placeholder}
+            isMulti={true}
+            onChange={(selectedOption) => {
+              const values = selectedOption.map(
+                (option: Option) => option.value,
+              );
+              field.onChange(values);
+              return;
+            }}
+            options={options}
+          />
+          <FormErrorMessage>
+            {errors[name]?.message?.toString()}
+          </FormErrorMessage>
+        </FormControl>
+      )}
+    />
+  );
+}
