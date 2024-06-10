728c9e67aff077dd7760bcc2ef4658c40e830ed0
Gabriel Di Vanna Camargo <zlFastShow@hotmail.com>
2024-06-10 16:05:51 -0300

    Update hooks and sorter

--- src/pages/institutional-events/hook.ts
+++ src/pages/institutional-events/hook.ts
@@ -1,13 +1,13 @@
 import { useToast } from '@chakra-ui/react';
-import { InstitutionalEvent } from 'models/common/institutionalEvent.model';
+import { InstitutionalEventResponse } from 'models/http/responses/instituionalEvent.response.models';
 import { useCallback, useEffect, useState } from 'react';
-import InstutionalEventsService from 'services/api/institutional-events.service';
+import InstutionalEventsService from 'services/api/institutionalEvents.service';
 
 const service = new InstutionalEventsService();
 
 export const useEvents = () => {
   const [loading, setLoading] = useState(false);
-  const [events, setEvents] = useState<InstitutionalEvent[]>([]);
+  const [events, setEvents] = useState<InstitutionalEventResponse[]>([]);
 
   const toast = useToast();
 
@@ -31,7 +31,7 @@ export const useEvents = () => {
   }, [toast]);
 
   const deleteEvent = useCallback(
-    async (id: string) => {
+    async (id: number) => {
       try {
         await service.delete(id);
 
