d1cdbe2c2de55556a70d1cd387c307ad0cc4b5df
Gabriel Di Vanna Camargo <zlFastShow@hotmail.com>
2024-06-10 11:34:04 -0300

    Create schedule models

---
+++ src/pages/schedules/ScheduleModal/schedule.modal.interface.ts
@@ -0,0 +1,19 @@
+import { Recurrence } from 'utils/enums/recurrence.enum';
+
+export interface ScheduleModalProps {}
+
+interface ScheduleFormBase {
+  calendar_id: number;
+  start_date: string;
+  end_date: string;
+  recurrence: Recurrence;
+  skip_exceptions: boolean;
+  all_day: boolean;
+  allocated?: boolean;
+}
+
+export interface ScheduleForm extends ScheduleFormBase {
+  week_day: string;
+  start_time: string;
+  end_time: string;
+}
