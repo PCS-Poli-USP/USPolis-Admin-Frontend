e427a96352a1b08afca56c52488abb32cfe6f1b7
Gabriel Di Vanna Camargo <zlFastShow@hotmail.com>
2024-07-01 20:12:13 -0300

    Fix select component

--- src/components/common/form/Select/index.tsx
+++ src/components/common/form/Select/index.tsx
@@ -7,7 +7,7 @@ import {
   Spinner,
 } from '@chakra-ui/react';
 import { FieldProps } from '../form.interface';
-import { useFormContext } from 'react-hook-form';
+import { Controller, useFormContext } from 'react-hook-form';
 
 type Option = {
   label: string;
@@ -25,7 +25,6 @@ export function Select({
   name,
   options,
   disabled = false,
-  value = undefined,
   placeholder = undefined,
   isLoading = false,
   mt = undefined,
@@ -35,14 +34,42 @@ export function Select({
   onChange = undefined,
 }: SelectProps) {
   const {
-    register,
+    control,
     formState: { errors },
   } = useFormContext();
 
   return (
     <FormControl isInvalid={!!errors[name]} mt={mt} mb={mb} ml={ml} mr={mr}>
       <FormLabel alignSelf='flex-start'>{label}</FormLabel>
-      <ChakraSelect
+      <Controller
+        name={name}
+        control={control}
+        render={({ field }) => (
+          <ChakraSelect
+            {...field}
+            id={name}
+            value={field.value ? field.value : ''}
+            disabled={disabled || isLoading}
+            icon={isLoading ? <Spinner /> : <ChevronDownIcon />}
+            onChange={(event) => {
+              if (onChange) onChange(event);
+              field.onChange(event.target.value);
+            }}
+          >
+            {placeholder ? (
+              <option value={undefined}>{placeholder}</option>
+            ) : (
+              <option value={undefined}>Selecione uma opção</option>
+            )}
+            {options.map((option) => (
+              <option key={option.value} value={option.value}>
+                {option.label}
+              </option>
+            ))}
+          </ChakraSelect>
+        )}
+      />
+      {/* <ChakraSelect
         {...register(name)}
         disabled={disabled || isLoading}
         value={value}
@@ -60,7 +87,7 @@ export function Select({
             {opt.label}
           </option>
         ))}
-      </ChakraSelect>
+      </ChakraSelect> */}
       <FormErrorMessage>{errors[name]?.message?.toString()}</FormErrorMessage>
     </FormControl>
   );
