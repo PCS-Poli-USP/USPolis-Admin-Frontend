2d38e39031db250b95e7b7e8f6c3f4593cac4e13
gdvcamargo <zlFastShow@hotmail.com>
2024-11-12 22:58:26 -0300

    Create autentication mock

--- src/components/routes/private.route.tsx
+++ src/components/routes/private.route.tsx
@@ -1,9 +1,12 @@
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
 
+const OVERRIDE = process.env.REACT_APP_OVERRIDE_AUTH;
+
 const PrivateRoute: React.FC = () => {
   const location = useLocation();
+  console.log(OVERRIDE);
   const token = localStorage.getItem('access_token'); // Check for token in localStorage or sessionStorage
-  return token ? (
+  return token || OVERRIDE === 'true' ? (
     <Outlet />
   ) : (
     <Navigate to='/auth' replace={true} state={{ from: location }} /> // To redirects after login
