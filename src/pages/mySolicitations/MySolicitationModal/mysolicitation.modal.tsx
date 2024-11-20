695a83ded8deb42be15c1883f9a99667d25351b3
gdvcamargo <zlFastShow@hotmail.com>
2024-11-20 17:33:45 -0300

    Create mobile components for my solicitations page

---
+++ src/pages/mySolicitations/MySolicitationModal/mysolicitation.modal.tsx
@@ -0,0 +1,170 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Text,
+  Box,
+  Heading,
+  HStack,
+  VStack,
+} from '@chakra-ui/react';
+import { ModalProps } from 'models/interfaces';
+import { ClassroomSolicitationResponse } from 'models/http/responses/classroomSolicitation.response.models';
+import { ReservationType } from 'utils/enums/reservations.enum';
+import moment from 'moment';
+
+interface MySoliciationModalProps extends ModalProps {
+  solicitation?: ClassroomSolicitationResponse;
+}
+
+function MySolicitationModal({
+  isOpen,
+  onClose,
+  solicitation,
+}: MySoliciationModalProps) {
+  return (
+    <>
+      {solicitation ? (
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader textColor={'black'}>Reserva de Sala</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <VStack align={'flex-start'} spacing={4}>
+                <Box>
+                  <Heading
+                    size='sm'
+                    textTransform='uppercase'
+                    textColor={'black'}
+                  >
+                    Solicitante
+                  </Heading>
+                  <Text pt='2' fontSize='md' textColor={'black'}>
+                    {`${solicitation.user}`}
+                  </Text>
+                  <Text pt='2' fontSize='md' textColor={'black'}>
+                    {`${solicitation.email}`}
+                  </Text>
+                </Box>
+                <Box>
+                  <Heading
+                    size='sm'
+                    textTransform='uppercase'
+                    textColor={'black'}
+                  >
+                    {`Motivo - ${ReservationType.translate(
+                      solicitation.reservation_type,
+                    )}`}
+                  </Heading>
+                  <Text
+                    pt='2'
+                    fontSize='md'
+                    textColor={'black'}
+                    textAlign={'justify'}
+                  >
+                    {solicitation.reason
+                      ? solicitation.reason
+                      : 'Descrição não informada.'}
+                  </Text>
+                </Box>
+                <Box>
+                  <Heading
+                    size='sm'
+                    textTransform='uppercase'
+                    textColor={'black'}
+                  >
+                    Local, Horário e Datas
+                  </Heading>
+                  <Text pt='2' fontSize='md' textColor={'black'}>
+                    {`Local: ${solicitation.building}, sala ${
+                      solicitation.classroom
+                        ? solicitation.classroom
+                        : 'NÃO ESPECIFICADA'
+                    }`}
+                    <br />
+                    {`Início: ${
+                      solicitation.start_time
+                        ? moment(solicitation.start_time, 'HH:mm').format(
+                            'HH:mm',
+                          )
+                        : 'NÃO ESPECIFICADO'
+                    }`}
+                    <br />
+                    {`Fim: ${
+                      solicitation.end_time
+                        ? moment(solicitation.end_time, 'HH:mm').format('HH:mm')
+                        : 'NÃO ESPECIFICADO'
+                    }`}
+                    <br />
+                    {`Dias: ${solicitation.dates
+                      .map((date) => moment(date).format('DD/MM/YYYY'))
+                      .join(', ')}`}
+                  </Text>
+                  <HStack
+                    align={'center'}
+                    alignContent={'center'}
+                    alignItems={'center'}
+                    verticalAlign={'center'}
+                  >
+                    <Text textColor={'red.500'} mt={1}>
+                      {solicitation.required_classroom
+                        ? 'Obrigatóriamente essa sala'
+                        : ''}
+                    </Text>
+                  </HStack>
+                </Box>
+                <Box>
+                  <Heading
+                    size='sm'
+                    textTransform='uppercase'
+                    textColor={'black'}
+                  >
+                    Requisitos
+                  </Heading>
+                  <Text pt='2' fontSize='md' textColor={'black'}>
+                    {`Capacidade para ${solicitation.capacity} pessoas`}
+                  </Text>
+                </Box>
+                <Box>
+                  <Heading
+                    size='sm'
+                    textTransform='uppercase'
+                    textColor={'black'}
+                  >
+                    Situação
+                  </Heading>
+                  <Text
+                    pt='2'
+                    fontSize='md'
+                    textColor={
+                      solicitation.approved
+                        ? 'green.500'
+                        : solicitation.denied
+                        ? 'red.500'
+                        : 'yellow.500'
+                    }
+                  >
+                    {solicitation.approved
+                      ? 'Aprovado'
+                      : solicitation.denied
+                      ? 'Negado'
+                      : 'Pendente'}
+                  </Text>
+                </Box>
+              </VStack>
+            </ModalBody>
+
+            <ModalFooter></ModalFooter>
+          </ModalContent>
+        </Modal>
+      ) : undefined}
+    </>
+  );
+}
+
+export default MySolicitationModal;
---
+++ src/pages/mySolicitations/MySolicitationStack/mysolicitation.stack.tsx
@@ -0,0 +1,165 @@
+import { Box, Checkbox, HStack, StackDivider, VStack } from '@chakra-ui/react';
+import Select, { SelectInstance } from 'react-select';
+import { ClassroomSolicitationResponse } from 'models/http/responses/classroomSolicitation.response.models';
+import { useEffect, useRef, useState } from 'react';
+import SolicitationStackBody from './mysolicitation.stack.body';
+
+interface SolicitationStackProps {
+  solicitations: ClassroomSolicitationResponse[];
+  handleOnClick: (data: ClassroomSolicitationResponse) => void;
+  reset: () => void;
+}
+
+type Option = {
+  value: string;
+  label: string;
+};
+
+function SolicitationStack({
+  solicitations,
+  handleOnClick,
+  reset,
+}: SolicitationStackProps) {
+  const [hidden, setHidden] = useState(true);
+  const [current, setCurrent] = useState<ClassroomSolicitationResponse[]>([]);
+  const [filtered, setFiltered] = useState<ClassroomSolicitationResponse[]>([]);
+  const [buildingSearch, setBuildingSearch] = useState('');
+  const [classroomSearch, setClassroomSearch] = useState<string>();
+  const [classroomOptions, setClassroomOptions] = useState<Option[]>([]);
+  const selectRef = useRef<SelectInstance<Option>>(null);
+
+  function filterSolicitation(building: string, classroom: string | undefined) {
+    let newCurrent = [...current];
+    if (building)
+      newCurrent = newCurrent.filter((val) =>
+        val.building.toLowerCase().includes(building.toLowerCase()),
+      );
+    if (classroom)
+      newCurrent = newCurrent.filter((val) =>
+        val.classroom
+          ? val.classroom.toLowerCase().includes(classroom.toLowerCase())
+          : 'não especificada'.includes(classroom.toLowerCase()),
+      );
+
+    setFiltered(newCurrent);
+  }
+
+  useEffect(() => {
+    if (hidden) {
+      const initial = [
+        ...solicitations.filter((solicitation) => !solicitation.closed),
+      ];
+      setCurrent(initial);
+      setFiltered((prev) => prev.filter((val) => !val.closed));
+    } else {
+      setCurrent([...solicitations]);
+      setFiltered((prev) => {
+        // if (prev.length === 0) return [...solicitations];
+        return prev;
+      });
+    }
+  }, [hidden, solicitations]);
+
+  const buildingOptions = solicitations
+    .map((solicitation) => ({
+      value: solicitation.building,
+      label: solicitation.building,
+    }))
+    .filter(
+      (value, index, self) =>
+        self.findIndex((v) => v.value === value.value) === index,
+    )
+    .sort((a, b) => a.value.localeCompare(b.value));
+
+  useEffect(() => {
+    const options = solicitations
+      .filter((val) => val.building === buildingSearch)
+      .map((solicitation) => ({
+        value: solicitation.classroom
+          ? solicitation.classroom
+          : 'Não especificada',
+        label: solicitation.classroom
+          ? solicitation.classroom
+          : 'Não especificada',
+      }))
+      .filter(
+        (value, index, self) =>
+          self.findIndex((v) => v.value === value.value) === index,
+      )
+      .sort((a, b) => a.value.localeCompare(b.value));
+    setClassroomOptions(options);
+  }, [buildingSearch, solicitations]);
+
+  return (
+    <VStack
+      divider={<StackDivider border={'1px solid lightgray'} />}
+      alignItems={'flex-start'}
+      alignContent={'flex-start'}
+      w={'full'}
+      spacing={2}
+      paddingRight={5}
+    >
+      <HStack w={'full'}>
+        <Box w={'50%'}>
+          <Select
+            placeholder='Filtrar por Prédio'
+            value={
+              buildingSearch
+                ? { label: buildingSearch, value: buildingSearch }
+                : undefined
+            }
+            isClearable={true}
+            isSearchable={true}
+            options={buildingOptions}
+            onChange={(option) => {
+              if (option) {
+                setBuildingSearch(option.value);
+                if (selectRef.current) selectRef.current.clearValue();
+                setClassroomSearch(undefined);
+                filterSolicitation(option.value, undefined);
+              }
+            }}
+          />
+        </Box>
+        <Box w={'50%'}>
+          <Select
+            ref={selectRef}
+            placeholder='Filtrar por Sala'
+            value={
+              classroomSearch
+                ? { label: classroomSearch, value: classroomSearch }
+                : undefined
+            }
+            isClearable={true}
+            isSearchable={true}
+            isDisabled={buildingSearch === ''}
+            options={classroomOptions}
+            onChange={(option) => {
+              if (option) {
+                setClassroomSearch(option.value);
+                filterSolicitation(buildingSearch, option.value);
+              }
+            }}
+          />
+        </Box>
+      </HStack>
+      <HStack>
+        <Checkbox
+          isChecked={hidden}
+          onChange={() => {
+            setHidden((prev) => !prev);
+          }}
+        >
+          Ocultar aprovados/negados
+        </Checkbox>
+      </HStack>
+      <SolicitationStackBody
+        reset={reset}
+        solicitations={buildingSearch || classroomSearch ? filtered : current}
+        handleOnClick={handleOnClick}
+      />
+    </VStack>
+  );
+}
+
+export default SolicitationStack;
