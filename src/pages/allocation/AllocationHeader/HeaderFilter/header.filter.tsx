8c6a9398f30cc7777e17b2bb7f2779d77f3c1974
gdvcamargo <zlFastShow@hotmail.com>
2024-11-21 19:59:04 -0300

    Make allocation page responsive

---
+++ src/pages/allocation/AllocationHeader/HeaderFilter/header.filter.tsx
@@ -0,0 +1,228 @@
+import { Box, Flex, Text } from '@chakra-ui/react';
+import Select, { StylesConfig } from 'react-select';
+import { AllocationHeaderProps } from '..';
+import { AllocationEnum } from 'utils/enums/allocation.enum';
+
+type OptionType = { value: string; label: string };
+
+const customStyles: StylesConfig<OptionType, false> = {
+  menu: (provided) => ({
+    ...provided,
+    maxHeight: '200px', // Altura máxima do menu
+    overflowY: 'auto', // Permite o scroll vertical
+  }),
+  menuList: (provided) => ({
+    ...provided,
+    maxHeight: '200px', // Altura máxima da lista
+    overflowY: 'auto', // Ativa o scroll
+    zIndex: 7,
+  }),
+};
+
+function HeaderFilter({
+  buildingSearchValue,
+  setBuildingSearchValue,
+  classroomSearchValue,
+  setClassroomSearchValue,
+  nameSearchValue,
+  setNameSearchValue,
+  classSearchValue,
+  setClassSearchValue,
+  filterEvents,
+  events,
+  resources,
+}: AllocationHeaderProps) {
+  const buildingOptions = events
+    .map((event) => ({
+      value:
+        event.extendedProps.class_data?.building ||
+        AllocationEnum.UNALLOCATED_BUILDING_ID,
+      label:
+        event.extendedProps.class_data?.building ||
+        AllocationEnum.UNALLOCATED_BUILDING_ID,
+    }))
+    .filter(
+      (value, index, self) =>
+        self.findIndex((v) => v.value === value.value) === index,
+    )
+    .sort((a, b) => a.value.localeCompare(b.value));
+
+  const classroomsOptions = events
+    .map((event) => ({
+      value:
+        event.extendedProps.class_data?.classroom ||
+        AllocationEnum.UNALLOCATED_CLASSROOM_ID,
+      label:
+        event.extendedProps.class_data?.classroom ||
+        AllocationEnum.UNALLOCATED_CLASSROOM_ID,
+    }))
+    .filter(
+      (value, index, self) =>
+        self.findIndex((v) => v.value === value.value) === index,
+    )
+    .sort((a, b) => a.value.localeCompare(b.value));
+
+  const subjectOptions = events
+    .map((event) => ({
+      value: event.extendedProps.class_data?.subject_name || event.title,
+      label: event.extendedProps.class_data
+        ? `${event.extendedProps.class_data?.subject_code} - ${event.extendedProps.class_data?.subject_name}`
+        : event.title,
+    }))
+    .filter(
+      (value, index, self) =>
+        self.findIndex((v) => v.value === value.value) === index,
+    )
+    .sort((a, b) => a.value.localeCompare(b.value));
+
+  const classOptions = events
+    .filter(
+      (event) =>
+        event.extendedProps.class_data &&
+        event.extendedProps.class_data.subject_name === nameSearchValue,
+    )
+    .map((event) => ({
+      value: event.extendedProps.class_data?.code || '',
+      label: event.extendedProps.class_data?.code.slice(-2) || '',
+    }))
+    .filter(
+      (value, index, self) =>
+        self.findIndex((v) => v.value === value.value) === index,
+    )
+    .sort((a, b) => a.value.localeCompare(b.value));
+
+  return (
+    <Flex direction={'column'} gap={2}>
+      <Text fontWeight={'bold'}>Filtros: </Text>
+      <Flex direction={'row'} gap={2} w={'full'}>
+        <Box w={'50%'}>
+          <Select
+            selectedOptionColorScheme={'purple'}
+            placeholder='Prédio'
+            isClearable={true}
+            options={buildingOptions}
+            value={
+              buildingSearchValue
+                ? { value: buildingSearchValue, label: buildingSearchValue }
+                : undefined
+            }
+            onChange={(option: OptionType) => {
+              if (option) {
+                setBuildingSearchValue(option.value);
+                filterEvents(
+                  option.value,
+                  classroomSearchValue,
+                  nameSearchValue,
+                  classSearchValue,
+                );
+              } else {
+                setBuildingSearchValue('');
+                filterEvents(
+                  '',
+                  classroomSearchValue,
+                  nameSearchValue,
+                  classSearchValue,
+                );
+              }
+            }}
+          />
+        </Box>
+        <Box w={'50%'}>
+          <Select
+            placeholder='Sala'
+            isClearable={true}
+            options={classroomsOptions}
+            value={
+              classroomSearchValue
+                ? { value: classroomSearchValue, label: classroomSearchValue }
+                : undefined
+            }
+            onChange={(option: OptionType) => {
+              if (option) {
+                setClassroomSearchValue(option.value);
+                filterEvents(
+                  buildingSearchValue,
+                  option.value,
+                  nameSearchValue,
+                  classSearchValue,
+                );
+              } else {
+                setClassroomSearchValue('');
+                filterEvents(
+                  buildingSearchValue,
+                  '',
+                  nameSearchValue,
+                  classSearchValue,
+                );
+              }
+            }}
+          />
+        </Box>
+      </Flex>
+      <Flex direction={'column'} gap={2} w={'full'}>
+        <Box w={'100%'}>
+          <Select
+            placeholder='Disciplina'
+            isClearable={true}
+            value={
+              nameSearchValue
+                ? { value: nameSearchValue, label: nameSearchValue }
+                : undefined
+            }
+            options={subjectOptions}
+            onChange={(option: OptionType) => {
+              if (option) {
+                setNameSearchValue(option.value);
+                filterEvents(
+                  buildingSearchValue,
+                  classroomSearchValue,
+                  option.value,
+                  classSearchValue,
+                );
+              } else {
+                setNameSearchValue('');
+                setClassSearchValue('');
+                filterEvents(buildingSearchValue, classroomSearchValue, '', '');
+              }
+            }}
+          />
+        </Box>
+        <Box w={'100%'} h={'fit-content'}>
+          <Select
+            styles={customStyles}
+            isDisabled={!nameSearchValue}
+            placeholder='Turma'
+            isClearable={true}
+            value={
+              classSearchValue
+                ? { value: classSearchValue, label: classSearchValue }
+                : undefined
+            }
+            options={classOptions}
+            onChange={(option: OptionType | null) => {
+              if (option) {
+                setClassSearchValue(option.value);
+                filterEvents(
+                  buildingSearchValue,
+                  classroomSearchValue,
+                  nameSearchValue,
+                  option.value,
+                );
+              } else {
+                setClassSearchValue('');
+                filterEvents(
+                  buildingSearchValue,
+                  classroomSearchValue,
+                  nameSearchValue,
+                  '',
+                );
+              }
+            }}
+          />
+        </Box>
+      </Flex>
+    </Flex>
+  );
+}
+
+export default HeaderFilter;
---
+++ src/pages/allocation/CustomCalendar/EventModal/event.modal.tsx
@@ -0,0 +1,169 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Text,
+  VStack,
+  Box,
+} from '@chakra-ui/react';
+import { ModalProps } from 'models/interfaces';
+import { EventApi } from '@fullcalendar/core';
+import { EventExtendedProps } from 'pages/allocation/interfaces/allocation.interfaces';
+import moment from 'moment';
+import { getEventScheduleText } from 'pages/allocation/utils/allocation.formatter';
+
+interface EventModalProps extends ModalProps {
+  event?: EventApi;
+}
+
+function EventModal({ isOpen, onClose, event }: EventModalProps) {
+  const extendedProps: EventExtendedProps | undefined = event ? event?.extendedProps : undefined;
+  const classData = extendedProps ? extendedProps.class_data : undefined;
+  const reservationData = extendedProps
+    ? extendedProps.reservation_data
+    : undefined;
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} closeOnOverlayClick={false}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>
+          {event ? event.title : ''}
+          <Text>{classData ? classData.subject_name : ''}</Text>
+        </ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <VStack align={'flex-start'} spacing={2}>
+            {classData ? (
+              <>
+                <VStack spacing={'2px'} alignItems='start'>
+                  <Text fontSize='xl' textColor='#408080' fontWeight='bold'>
+                    {`Turma ${classData.code.slice(-2)}`}
+                  </Text>
+                </VStack>
+                <Box>
+                  <Text fontSize='xl' textColor='#408080' fontWeight='bold'>
+                    Professores
+                  </Text>
+                  {(classData.professors as string[]).map(
+                    (professor, index) => (
+                      <Text fontSize='lg' textColor='#408080' key={index}>
+                        {professor}
+                      </Text>
+                    ),
+                  )}
+                </Box>
+
+                <Box>
+                  <Text fontSize='xl' textColor='#408080' fontWeight='bold'>
+                    Informações
+                  </Text>
+                  <Text fontSize='lg' textColor='#408080'>{`Sala: ${
+                    classData.classroom ? classData.classroom : 'NÃO ALOCADA'
+                  }`}</Text>
+
+                  <Text fontSize='lg' textColor='#408080'>
+                    {extendedProps ? getEventScheduleText(extendedProps) : ''}
+                  </Text>
+
+                  <Text fontSize='lg' textColor='#408080'>
+                    {`Horário: ${classData.start_time.substring(
+                      0,
+                      5,
+                    )} ~ ${classData.end_time.substring(0, 5)}`}
+                  </Text>
+
+                  <Text
+                    fontSize='lg'
+                    textColor='#408080'
+                    hidden={classData.start_date ? false : true}
+                  >
+                    {`De ${
+                      classData.start_date
+                        ? moment(classData.start_date).format('DD/MM/YYYY')
+                        : ''
+                    } até ${
+                      classData.end_date
+                        ? moment(classData.end_date).format('DD/MM/YYYY')
+                        : ''
+                    }`}
+                  </Text>
+
+                  <Text
+                    fontSize='lg'
+                    textColor='#408080'
+                  >{`Inscritos: ${classData.subscribers}`}</Text>
+                </Box>
+              </>
+            ) : undefined}
+
+            {reservationData ? (
+              <>
+                <Text fontSize='xl' textColor='#408080' fontWeight='bold'>
+                  {`Reserva - ${reservationData.title}`}
+                </Text>
+
+                <Box>
+                  <Text fontSize='xl' textColor='#408080' fontWeight='bold'>
+                    Motivo
+                  </Text>
+                  <Text fontSize='lg' textColor='#408080'>
+                    {reservationData.reason
+                      ? reservationData.reason
+                      : 'Não informada'}
+                  </Text>
+
+                  <Text fontSize='xl' fontWeight='bold' textColor='#408080'>
+                    Local
+                  </Text>
+                  <Text fontSize='lg' textColor='#408080'>
+                    {`${reservationData.building} - ${reservationData.classroom}`}
+                  </Text>
+
+                  <Text fontSize='xl' fontWeight='bold' textColor='#408080'>
+                    Horários
+                  </Text>
+                  <Text fontSize='lg' textColor='#408080'>
+                    {extendedProps ? getEventScheduleText(extendedProps) : ''}
+                  </Text>
+
+                  <Text fontSize='lg' textColor='#408080'>
+                    {`Horário: ${reservationData.start_time.substring(
+                      0,
+                      5,
+                    )} ~ ${reservationData.end_time.substring(0, 5)}`}
+                  </Text>
+
+                  <Text
+                    fontSize='lg'
+                    textColor='#408080'
+                    hidden={reservationData.start_date ? false : true}
+                  >
+                    {`De ${
+                      reservationData.start_date
+                        ? moment(reservationData.start_date).format(
+                            'DD/MM/YYYY',
+                          )
+                        : ''
+                    } até ${
+                      reservationData.end_date
+                        ? moment(reservationData.end_date).format('DD/MM/YYYY')
+                        : ''
+                    }`}
+                  </Text>
+                </Box>
+              </>
+            ) : undefined}
+          </VStack>
+        </ModalBody>
+
+        <ModalFooter></ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+}
+
+export default EventModal;
