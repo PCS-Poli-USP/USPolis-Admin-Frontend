db18fe41c245ea6d9236eacbafc551f9f89b2293
gdvcamargo <zlFastShow@hotmail.com>
2025-02-20 20:21:49 -0300

    Update calendar responses

--- src/models/http/responses/holidayCategory.response.models.ts
+++ src/models/http/responses/holidayCategory.response.models.ts
@@ -2,6 +2,7 @@ import { HolidayResponse } from './holiday.response.models';
 
 export interface HolidayCategoryResponse {
   id: number;
+  owner_id: number;
   name: string;
   created_by: string;
   holidays: HolidayResponse[];
