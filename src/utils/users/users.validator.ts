34d5e4a20484cb35cbf2b45b66df15c72753289b
gdvcamargo <zlFastShow@hotmail.com>
2025-03-20 23:51:10 -0300

    Move table utils for user to user validators

--- src/utils/users/users.validator.ts
+++ src/utils/users/users.validator.ts
@@ -1,11 +1,42 @@
-export function nameInvalid(name: string) {
-  return name.length <= 0;
-}
+import { ClassResponse } from 'models/http/responses/class.response.models';
+import { UserResponse } from 'models/http/responses/user.response.models';
+import CommonValidator from 'utils/common/common.validator';
 
-export function emailInvalid(email: string) {
-  return email.length <= 0 || !email.includes('@') || !email.includes('.');
-}
+export class UsersValidator extends CommonValidator {
+  static isInvalidName(name: string) {
+    return this.isEmptyString(name);
+  }
+  static isAdmin(user: UserResponse | null) {
+    return user ? user.is_admin : false;
+  }
 
-export function buildingsInvalid(buildings: number[], is_admin: boolean) {
-  return buildings.length <= 0 && !is_admin;
+  static checkUserPermission(
+    user: UserResponse | null,
+    buildings_ids: number[],
+  ): boolean {
+    if (!user) return false;
+    if (user.is_admin) return true;
+    if (!user.buildings) return false;
+    const userSet = new Set(user.buildings.map((b) => b.id));
+    const buildingsSet = new Set<number>(buildings_ids);
+    // Check if buildingsSet is a subset of userSet
+    for (let buildingId of buildingsSet) {
+      if (!userSet.has(buildingId)) {
+        return false;
+      }
+    }
+    return true;
+  }
+  static checkUserPermissionOnClass(
+    user: UserResponse | null,
+    cls: ClassResponse,
+  ): boolean {
+    if (!user) return false;
+    const class_allocated_buildings_ids: number[] = [];
+    cls.schedules.forEach((schedule) => {
+      if (schedule.building_id)
+        class_allocated_buildings_ids.push(schedule.building_id);
+    });
+    return this.checkUserPermission(user, class_allocated_buildings_ids);
+  }
 }
