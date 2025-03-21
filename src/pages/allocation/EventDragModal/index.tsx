ec1a129f190d48c1db3f8ebfdb67b70519263362
gdvcamargo <zlFastShow@hotmail.com>
2025-03-20 23:53:25 -0300

    Finish EventDragModal

--- src/pages/allocation/EventDragModal/index.tsx
+++ src/pages/allocation/EventDragModal/index.tsx
@@ -17,31 +17,72 @@ import {
   VStack,
 } from '@chakra-ui/react';
 import { EventDropArg } from '@fullcalendar/core';
-import { EventExtendedProps } from 'models/http/responses/allocation.response.models';
+import {
+  ClassExtendedData,
+  EventExtendedProps,
+  ReservationExtendedData,
+} from 'models/http/responses/allocation.response.models';
 import { ModalProps } from 'models/interfaces';
-import moment, { Moment } from 'moment';
 import { useEffect, useState } from 'react';
 import { classNumberFromClassCode } from 'utils/classes/classes.formatter';
+import useAllocation from '../hooks/useAllocation';
+import { EventUpdate } from 'models/http/requests/allocation.request.models';
+import { AllocationEnum } from 'utils/enums/allocation.enum';
+import moment from 'moment';
 
 interface EventDragModalProps extends ModalProps {
   dragEvent?: EventDropArg;
   handleCancel: () => void;
+  refetch: () => Promise<void>;
 }
 
 type RadioValue = 'all' | 'one';
 
 function EventDragModal(props: EventDragModalProps) {
+  const { updateEvent } = useAllocation(false, false);
+
   const [selectValue, setSelectValue] = useState<RadioValue>();
   const [hasError, setHasError] = useState(false);
-  const [start, setStart] = useState<Moment>();
-  const [end, setEnd] = useState<Moment>();
+  const [errorMsg, setErrorMsg] = useState('');
+  const [start, setStart] = useState<string>('');
+  const [end, setEnd] = useState<string>('');
   const [building, setBuilding] = useState<string>();
   const [classroom, setClassroom] = useState<string>();
 
+  function getStartTimeFromEvent(event: EventDropArg) {
+    if (event.event.startStr) {
+      return moment(event.event.startStr).format('HH:mm');
+    }
+    const data = event.event._def.extendedProps as EventExtendedProps;
+    if (data.class_data) {
+      return data.class_data.start_time;
+    }
+    if (data.reservation_data) {
+      return data.reservation_data.start_time;
+    }
+    return '';
+  }
+
+  function getEndTimeFromEvent(event: EventDropArg) {
+    if (event.event.endStr) {
+      return moment(event.event.endStr).format('HH:mm');
+    }
+    const data = event.event._def.extendedProps as EventExtendedProps;
+    if (data.class_data) {
+      return data.class_data.end_time;
+    }
+    if (data.reservation_data) {
+      return data.reservation_data.end_time;
+    }
+    return '';
+  }
+
   useEffect(() => {
     if (props.dragEvent) {
-      setStart(moment(props.dragEvent.event.startStr));
-      setEnd(moment(props.dragEvent.event.endStr));
+      const start = getStartTimeFromEvent(props.dragEvent);
+      const end = getEndTimeFromEvent(props.dragEvent);
+      setStart(start.substring(0, 5));
+      setEnd(end.substring(0, 5));
 
       const values = props.dragEvent.event._def.resourceIds;
       if (values && values.length > 0) {
@@ -60,13 +101,74 @@ function EventDragModal(props: EventDragModalProps) {
     }
   }, [props.dragEvent]);
 
+  function getEventUpdateInput(
+    selectValue: RadioValue,
+    start: string,
+    end: string,
+    classroom: string,
+    building: string,
+    data: ClassExtendedData | ReservationExtendedData,
+  ): EventUpdate {
+    return {
+      desalocate:
+        classroom === AllocationEnum.UNALLOCATED &&
+        building === AllocationEnum.UNALLOCATED,
+      all_occurrences: selectValue === 'all',
+      start_time: start,
+      end_time: end,
+      building: building,
+      classroom: classroom,
+      schedule_id: data.schedule_id,
+      occurrence_id: data.occurrence_id,
+    };
+  }
+
   async function handleSubmit() {
     if (!selectValue) {
       setHasError(true);
+      setErrorMsg('Selecione uma opção');
       return;
     }
-    console.log(selectValue);
-    console.log(props.dragEvent);
+    if (!building || !classroom) {
+      setErrorMsg('Sala ou prédio inválido');
+      return;
+    }
+    if (!props.dragEvent) {
+      setHasError(true);
+      setErrorMsg('Evento inválido');
+      return;
+    }
+    if (!start || !end) {
+      setHasError(true);
+      setErrorMsg('Um ou mais horários inválidos');
+      return;
+    }
+    const extended = props.dragEvent.event._def
+      .extendedProps as EventExtendedProps;
+    if (!extended.class_data && !extended.reservation_data) {
+      setHasError(true);
+      setErrorMsg('Evento inválido');
+      return;
+    }
+    const dataInput = extended.class_data || extended.reservation_data;
+    if (!dataInput) {
+      setHasError(true);
+      setErrorMsg('Dados do evento inválidos');
+      return;
+    }
+    const data = getEventUpdateInput(
+      selectValue,
+      start,
+      end,
+      classroom,
+      building,
+      dataInput,
+    );
+    await updateEvent(data);
+    setHasError(false);
+    setSelectValue(undefined);
+    props.refetch();
+    props.onClose();
   }
 
   function getEventTitle(arg: EventDropArg) {
@@ -119,8 +221,8 @@ function EventDragModal(props: EventDragModalProps) {
                 {classroom ? classroom : 'Não encontrada'}
               </Text>
               <Text>
-                <b>Horário:</b> {start && start.format('HH:mm')} -{' '}
-                {end && end.format('HH:mm')}
+                <b>Horário:</b> {start ? start : 'Hora inválida'} -{' '}
+                {end ? end : 'Hora inválida'}
               </Text>
 
               <Heading size={'md'}>Opções</Heading>
@@ -139,11 +241,11 @@ function EventDragModal(props: EventDragModalProps) {
                     <Radio value='all' size={'lg'}>
                       Todos as ocorrências
                     </Radio>
-                    {!hasError ? undefined : (
-                      <FormErrorMessage>Selecione uma opção</FormErrorMessage>
-                    )}
                   </VStack>
                 </RadioGroup>
+                {!hasError ? undefined : (
+                  <FormErrorMessage>{errorMsg}</FormErrorMessage>
+                )}
               </FormControl>
             </VStack>
           ) : (
@@ -156,6 +258,7 @@ function EventDragModal(props: EventDragModalProps) {
               Cancelar
             </Button>
             <Button
+              disabled={!selectValue || !props.dragEvent}
               colorScheme='blue'
               onClick={async () => {
                 await handleSubmit();
