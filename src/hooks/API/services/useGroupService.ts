f0c3e2bb03f7a2582403887d6bd18d7e9c9c890d
gdvcamargo <zlFastShow@hotmail.com>
2025-04-12 03:11:43 -0300

    Create Group hooks

---
+++ src/hooks/API/services/useGroupService.ts
@@ -0,0 +1,44 @@
+import { AxiosResponse } from 'axios';
+import useAxiosPrivate from '../axios/useAxiosPrivate';
+import { GroupResponse } from 'models/http/responses/group.response.models';
+import { JSONResponse } from 'models/http/responses/common.response.models';
+import {
+  GroupRequest,
+  GroupUpdate,
+} from 'models/http/requests/group.request.models';
+
+const useGroupService = () => {
+  const PREFIX = '/admin/groups';
+  const axios = useAxiosPrivate();
+
+  const list = (): Promise<AxiosResponse<Array<GroupResponse>>> => {
+    return axios.get(PREFIX);
+  };
+
+  const listById = (id: number): Promise<AxiosResponse<GroupResponse>> => {
+    return axios.get(`${PREFIX}/${id}`);
+  };
+  const create = (
+    input: GroupRequest,
+  ): Promise<AxiosResponse<JSONResponse>> => {
+    return axios.post(`${PREFIX}`, input);
+  };
+  const update = (
+    id: number,
+    input: GroupUpdate,
+  ): Promise<AxiosResponse<JSONResponse>> => {
+    return axios.patch(`${PREFIX}/${id}`, input);
+  };
+  const remove = (id: number): Promise<AxiosResponse<JSONResponse>> => {
+    return axios.delete(`${PREFIX}/${id}`);
+  };
+  return {
+    list,
+    listById,
+    create,
+    update,
+    remove,
+  };
+};
+
+export default useGroupService;
---
+++ src/hooks/useGroups.ts
@@ -0,0 +1,99 @@
+import { useCallback, useEffect, useState } from 'react';
+import useGroupService from './API/services/useGroupService';
+import { GroupResponse } from 'models/http/responses/group.response.models';
+import useCustomToast from './useCustomToast';
+import {
+  GroupRequest,
+  GroupUpdate,
+} from 'models/http/requests/group.request.models';
+
+const useGroups = (initialFetch = true) => {
+  const service = useGroupService();
+  const [loading, setLoading] = useState(false);
+  const [groups, setGroups] = useState<GroupResponse[]>([]);
+
+  const showToast = useCustomToast();
+
+  const getAllGroups = useCallback(async () => {
+    setLoading(true);
+    await service
+      .list()
+      .then((response) => {
+        setGroups(response.data);
+      })
+      .catch((error) => {
+        showToast('Erro', 'Erro ao carregar grupos', 'error');
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const createGroup = useCallback(async (data: GroupRequest) => {
+    setLoading(true);
+    await service
+      .create(data)
+      .then((response) => {
+        showToast('Sucesso', `Grupo criado com sucesso!`, 'success');
+        getAllGroups();
+      })
+      .catch((error) => {
+        showToast('Erro', `Erro ao criar grupo: ${error}`, 'error');
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const updateGroup = useCallback(async (id: number, data: GroupUpdate) => {
+    setLoading(true);
+    await service
+      .update(id, data)
+      .then((response) => {
+        showToast('Sucesso', `Grupo atualizado com sucesso!`, 'success');
+        getAllGroups();
+      })
+      .catch((error) => {
+        showToast('Erro', `Erro ao atualizar grupo: ${error}`, 'error');
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const deleteGroup = useCallback(async (id: number) => {
+    setLoading(true);
+    await service
+      .remove(id)
+      .then((response) => {
+        showToast('Sucesso', `Grupo removido com sucesso!`, 'success');
+        getAllGroups();
+      })
+      .catch((error) => {
+        showToast('Erro', `Erro ao remover grupo: ${error}`, 'error');
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    if (initialFetch) getAllGroups();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [initialFetch]);
+
+  return {
+    loading,
+    groups,
+    getAllGroups,
+    createGroup,
+    updateGroup,
+    deleteGroup,
+  };
+};
+
+export default useGroups;
