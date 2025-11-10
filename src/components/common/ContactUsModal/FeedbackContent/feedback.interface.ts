d7761b0eb0c39304540ed170830d7c8c9a2d38a9
gdvcamargo <zlFastShow@hotmail.com>
2025-11-09 21:32:36 -0300

    [feature]: create FeedbackContent components

---
+++ src/components/common/ContactUsModal/FeedbackContent/feedback.interface.ts
@@ -0,0 +1,11 @@
+import { UseFormReturn } from 'react-hook-form';
+
+export interface FeedbackForm {
+  title: string;
+  message: string;
+}
+
+export interface FeedbackContentProps {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  form: UseFormReturn<FeedbackForm, any, FeedbackForm>;
+}
