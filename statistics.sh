712a760d91f1fef34e2297f8bf7d07b9b721a03e
gdvcamargo <zlFastShow@hotmail.com>
2026-08-30 02:21:27 -0300

    feat: update statistics script to include documentation line counts

--- statistics.sh
+++ statistics.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-SKIP_FOLDERS=(-path ./.yarn -o -path ./build -o -path ./node_modules)
+SKIP_FOLDERS=(-path ./.yarn -o -path ./build -o -path ./node_modules -o -path ./docs)
 
-mapfile -t FILES < <(find . \( "${SKIP_FOLDERS[@]}" \) -prune -o \( -name "*.ts" -o -name "*.jsx" \)  -print)
+mapfile -t FILES < <(find . \( "${SKIP_FOLDERS[@]}" \) -prune -o \( -name "*.ts" -o -name "*.tsx" \)  -print)
 
 LINES_COUNT=0
 for file in "${FILES[@]}"; do
@@ -10,5 +10,14 @@ for file in "${FILES[@]}"; do
     ((LINES_COUNT += lines_in_file))
 done
 
+mapfile -t DOCS_FILES < <(find ./docs \( -name "*.ts" -o -name "*.tsx" -o -name "*.md" \) -print)
+
+DOCS_LINES_COUNT=0
+for file in "${DOCS_FILES[@]}"; do
+    lines_in_file=$(wc -l < "$file")
+    ((DOCS_LINES_COUNT += lines_in_file))
+done
+
 
 echo "Total lines of code: $LINES_COUNT"
+echo "Total lines of docs code: $DOCS_LINES_COUNT"
